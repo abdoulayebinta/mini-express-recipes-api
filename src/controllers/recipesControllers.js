facc90dc2025cff772b3aa5316f865b14f7c9083
abdoulayebinta <bahabdoulayebinta@gmail.com>
2022-08-17 12:34:01 +0000

    Implement save() route handler for POST request to api/v1/recipes

--- src/controllers/recipesControllers.js
+++ src/controllers/recipesControllers.js
@@ -1,5 +1,6 @@
 const service = require('../services/recipesServices');
 
+// Get all recipes controller
 const getAll = async (req, res, next) => {
   try {
     res.json({ data: await service.getAll() });
@@ -8,6 +9,32 @@ const getAll = async (req, res, next) => {
   }
 };
 
+// Save recipe controller
+const save = async (req, res, next) => {
+  try {
+    const {
+      name,
+      healthLabels,
+      cookTimeMinutes,
+      prepTimeMinutes,
+      ingredients,
+    } = req.body;
+
+    const newRecipe = {
+      name,
+      healthLabels: [...healthLabels],
+      cookTimeMinutes,
+      prepTimeMinutes,
+      ingredients: [...ingredients],
+    };
+
+    res.status(201).json({ data: await service.save(newRecipe) });
+  } catch (error) {
+    next(error);
+  }
+};
+
 module.exports = {
   getAll,
+  save,
 };
