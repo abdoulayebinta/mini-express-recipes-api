005890b349688ed851ccbd7a4ddf30f41bbc1d0f
abdoulayebinta <bahabdoulayebinta@gmail.com>
2022-08-17 12:00:11 +0000

    Import and mount the router in the recipe routers

--- src/routers/recipesRouters.js
+++ src/routers/recipesRouters.js
@@ -3,5 +3,8 @@ const express = require('express');
 const router = express.Router();
 
 // add route handlers here
+const { getAll } = require('../controllers/recipesControllers');
+
+router.get('/', getAll);
 
 module.exports = router;
