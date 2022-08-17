2671d67606d26db82eb63837add1dd6d66676719
abdoulayebinta <bahabdoulayebinta@gmail.com>
2022-08-17 12:37:44 +0000

    Import and Mount the save() router handler on the / path

--- src/routers/recipesRouters.js
+++ src/routers/recipesRouters.js
@@ -3,8 +3,9 @@ const express = require('express');
 const router = express.Router();
 
 // add route handlers here
-const { getAll } = require('../controllers/recipesControllers');
+const { getAll, save } = require('../controllers/recipesControllers');
 
 router.get('/', getAll);
+router.post('/', save);
 
 module.exports = router;
