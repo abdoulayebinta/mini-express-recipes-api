857d02fb2659211388e53ea870c9aeeac02991b7
abdoulayebinta <bahabdoulayebinta@gmail.com>
2022-08-17 22:10:53 +0000

    Secure API route for PUT and DELETE

--- src/routers/recipesRouters.js
+++ src/routers/recipesRouters.js
@@ -13,9 +13,12 @@ const {
 
 // Route `GET` and `POST` HTTP requests for `/`
 router.route('/').get(getAllRecipes).post(auth.authenticate(), saveRecipe);
-//router.route('/').get(getAllRecipes).post(auth.authenticate(), saveRecipe);
 
 // Route `GET`, `PUT` and `DELETE` HTTP requests for `/api/v1/recipes/:id`
-router.route('/:id').get(getRecipe).put(updateRecipe).delete(deleteRecipe);
+router
+  .route('/:id')
+  .get(getRecipe)
+  .put(auth.authenticate(), updateRecipe)
+  .delete(auth.authenticate(), deleteRecipe);
 
 module.exports = router;
