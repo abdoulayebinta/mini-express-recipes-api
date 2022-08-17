29fe572d57c01da5911234faa28450d2caa42808
abdoulayebinta <bahabdoulayebinta@gmail.com>
2022-08-17 12:21:31 +0000

    Save a new recipe to the database

--- src/services/recipesServices.js
+++ src/services/recipesServices.js
@@ -3,8 +3,21 @@ const path = require('path');
 
 const recipesFilePath = path.join(__dirname, '../../db/recipes.json');
 
+// Get all recipes data
 const getAll = async () => JSON.parse(await fs.readFile(recipesFilePath));
+
+// Save a new recipes to the database
+const save = async (recipe) => {
+  const recipes = await getAll();
+  recipe.id = recipes.length + 1;
+  recipes.push(recipe);
+
+  await fs.writeFile(recipesFilePath, JSON.stringify(recipes));
+
+  return recipe;
+};
 
 module.exports = {
   getAll,
+  save,
 };
